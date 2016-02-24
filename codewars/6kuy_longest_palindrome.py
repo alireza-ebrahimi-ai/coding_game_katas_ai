68c1f178482e5b772c2209bb44214a7c879ce3f5
waniz <waniz@mail.ru>
2016-02-24 16:43:27 +0300

    Added:6kuy_next_version

---
+++ codewars/6kuy_longest_palindrome.py
@@ -0,0 +1,23 @@
+def inversion(origina_string):
+    return origina_string[::-1]
+
+def longest_palindrome (s):
+    if len(s) == 0:
+        return 0
+    reverse_string = ''
+    palindrome_length = 1
+    for position_in_string in range(len(s)):
+
+
+
+
+
+
+
+
+
+
+
+
+
+
