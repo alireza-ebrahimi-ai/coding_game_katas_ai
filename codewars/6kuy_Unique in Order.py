3bc47a384643edfd9959e0d7ec8e7265e25d51d8
waniz <waniz@mail.ru>
2016-03-10 12:18:51 +0300

    Added: 6kuy_Unique in Order

---
+++ codewars/6kuy_Unique in Order.py
@@ -0,0 +1,19 @@
+def unique_in_order(iterable):
+    answer = []
+    if iterable == "":
+        return answer
+
+    answer.append(iterable[0])
+    position = 0
+    index = 0
+    while position < len(iterable):
+        if answer[index] != iterable[position]:
+            answer.append(iterable[position])
+            index += 1
+            position += 1
+        else:
+            position += 1
+    return answer
+
+
+print unique_in_order('AAAABBBCCDAABBB')
