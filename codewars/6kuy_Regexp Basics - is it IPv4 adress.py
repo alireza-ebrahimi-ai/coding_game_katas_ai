7096f8d93979469c090c1ffa7fd7c91056abea97
waniz <waniz@mail.ru>
2016-03-29 15:11:38 +0300

    Added: 7kuy Sum Factorial

--- codewars/6kuy_Regexp Basics - is it IPv4 adress.py
+++ codewars/6kuy_Regexp Basics - is it IPv4 adress.py
@@ -4,7 +4,6 @@ REGEX = compile(r'((\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.){4}$')
 
 def ipv4_address(address):
 
-    return bool(match(REGEX, address + '.'))
+  return bool(match(REGEX, address + '.'))
 
-print ipv4_address("127.0.0.1\n")
-
+print(ipv4_address("127.0.0.1\n"))
---
+++ codewars/7kuy_Sum Factorial.py
@@ -0,0 +1,15 @@
+def calc(number):
+    answer = 1
+    for i in range(1, number + 1):
+        answer = answer * i
+    return answer
+
+def sum_factorial(lst):
+    answer = 0
+    for i in range(0, len(lst)):
+        answer = answer + calc(lst[i])
+
+    return answer
+
+
+print sum_factorial([4, 6])
