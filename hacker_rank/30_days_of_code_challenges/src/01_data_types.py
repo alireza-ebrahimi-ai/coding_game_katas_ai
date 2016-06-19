76600cdf9800a61277bdfd5b53550a61ce75e20a
Dmitry <waniz@mail.ru>
2016-06-19 18:06:57 +0300

    HackerRank day01

---
+++ hacker_rank/30_days_of_code_challenges/src/01_data_types.py
@@ -0,0 +1,16 @@
+
+# Declare second integer, double, and String variables.
+i =4
+d = 4.0
+s = 'HackerRank '
+# Read and save an integer, double, and String to your variables.
+my_i = int(input())
+my_d = float(input())
+my_s = input()
+# Print the sum of both integer variables on a new line.
+print(int(i + my_i))
+# Print the sum of the double variables on a new line.
+print(d + my_d)
+# Concatenate and print the String variables on a new line
+# The 's' variable above should be printed first.
+print(s + my_s)
---
+++ hacker_rank/Algorithms/warmup/04_diagonal_difference.py
@@ -0,0 +1,6 @@
+n = int(input().strip())
+a = []
+for a_i in range(n):
+    a_t = [int(a_temp) for a_temp in input().strip().split(' ')]
+    a.append(a_t)
+
