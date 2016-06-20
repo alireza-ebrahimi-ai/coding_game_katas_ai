c747f82b06980d8503da4cd117ac7098c47cde84
Dmitry <waniz@mail.ru>
2016-06-20 15:37:43 +0300

    HackerRank day03 fixed

--- hacker_rank/30_days_of_code_challenges/src/03_intro_t-_conditional_statements.py
+++ hacker_rank/30_days_of_code_challenges/src/03_intro_t-_conditional_statements.py
@@ -2,9 +2,9 @@ N = int(input().strip())
 
 if N % 2 != 0:
     print("Weird")
-elif N % 2 == 0 and (N > 2 or N < 5):
+elif N % 2 == 0 and (N > 2 and N < 6):
     print("Not Weird")
-elif N % 2 == 0 and (N > 6 or N < 20):
+elif N % 2 == 0 and (N > 6 and N < 21):
     print("Weird")
 elif N % 2 == 0 and N > 20:
     print("Not Weird")
