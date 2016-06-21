9149903a7532d257422b2bb6a0bbbba04634bfd1
Dmitry <waniz@mail.ru>
2016-06-21 10:14:21 +0300

    HackerRank warmup04

--- hacker_rank/Algorithms/warmup/04_diagonal_difference.py
+++ hacker_rank/Algorithms/warmup/04_diagonal_difference.py
@@ -7,10 +7,10 @@ for a_i in range(n):
 primary_diagonal = 0
 secondary_diagonal = 0
 
-for i in range(0, n - 1):
-    primary_diagonal += a[i][i]
+for i in range(0, n):
+    primary_diagonal = primary_diagonal + a[i][i]
 
-for k in range(0, n - 1):
-    secondary_diagonal += a[n-k][n-1-k]
+for k in range(0, n):
+    secondary_diagonal = secondary_diagonal + a[n - k - 1][k]
 
 print(abs(primary_diagonal - secondary_diagonal))
