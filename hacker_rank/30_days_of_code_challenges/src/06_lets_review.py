d3914f64fd83fc20b546511183fb64a93cf060af
Dmitry Kustikov <waniz@mail.ru>
2016-08-01 12:17:00 +0300

    30 days: 05

---
+++ hacker_rank/30_days_of_code_challenges/src/06_lets_review.py
@@ -0,0 +1,5 @@
+"""
+Given a string, S of length N that is indexed from 0 to N-1, print its even-indexed and odd-indexed characters as 2
+space-separated strings on a single line (see the Sample below for more detail).
+"""
+
