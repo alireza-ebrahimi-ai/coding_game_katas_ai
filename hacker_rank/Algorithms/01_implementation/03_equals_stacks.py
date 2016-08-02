82da04460e8c7e01b462e2a9e5160e76832211b7
Dmitry Kustikov <waniz@mail.ru>
2016-08-02 17:14:48 +0300

    warmup complete

---
+++ hacker_rank/Algorithms/01_implementation/03_equals_stacks.py
@@ -0,0 +1,6 @@
+"""
+You have three stacks of cylinders where each cylinder has the same diameter,
+but they may vary in height.
+You can change the height of a stack by removing and discarding its topmost cylinder any number of times.
+"""
+
