7de0fa206605e7ae2229bfc0cb6efcae81cbe787
Dmitry Kustikov <waniz@mail.ru>
2016-08-04 18:20:58 +0300

    added some templates

---
+++ hacker_rank/Algorithms/01_implementation/07_bigger_is_greater.py
@@ -0,0 +1,20 @@
+"""
+Given a word w, rearrange the letters of w to construct another word s in such a way that s
+is lexicographically greater than w.
+In case of multiple possible answers, find the lexicographically smallest one among them.
+
+5
+ab
+bb
+hefg
+dhck
+dkhc
+
+=
+ba
+no answer
+hegf
+dhkc
+hcdk
+"""
+
