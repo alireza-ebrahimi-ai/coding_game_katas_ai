6e720f37c84923567bb4bf267c4ea981ce2e2678
Dmitry Kustikov <waniz@mail.ru>
2016-08-05 18:40:14 +0300

    added some stuff

---
+++ hacker_rank/Algorithms/01_implementation/09_save_the_prisoner.py
@@ -0,0 +1,16 @@
+"""
+
+A jail has N prisoners, and each prisoner has a unique id number,
+S , ranging from 1 to N. There are M sweets that must be distributed to the prisoners.
+
+1
+5 2 1
+
+= 2
+"""
+
+t = int(input().strip())
+n, m, s = map(int, input().strip().split(' '))
+
+
+
