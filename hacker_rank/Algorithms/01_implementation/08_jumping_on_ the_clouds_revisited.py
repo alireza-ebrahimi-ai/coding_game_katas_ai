b53e33cef99f3415292dc87dc7b2bf3bebe1cbd6
Dmitry Kustikov <waniz@mail.ru>
2016-08-05 17:33:58 +0300

    added:08

--- hacker_rank/Algorithms/01_implementation/08_jumping_on_ the_clouds_revisited.py
+++ hacker_rank/Algorithms/01_implementation/08_jumping_on_ the_clouds_revisited.py
@@ -1,7 +1,7 @@
 """
 Aerith is playing a cloud game!
-In this game, there are  clouds numbered sequentially from 0 to n-1. Each cloud is either an ordinary cloud or
-a thundercloud.
+In this game, there are  clouds numbered sequentially from 0 to n-1. Each cloud is either
+an ordinary cloud or a thundercloud.
 
 8 2
 0 0 1 0 0 1 1 0
@@ -9,22 +9,16 @@ a thundercloud.
 = 92
 """
 
-n, k = int(input().strip().split(' '))
+n, k = map(int, input().strip().split(' '))
 c = [int(c_temp) for c_temp in input().strip().split(' ')]
 
-step = 0
-pos = 0
-while pos < n:
-    if pos + 2 < n:
-        if c[pos + 2] != 1:
-            pos += 2
-            step += 1
-        else:
-            pos += 1
-            step += 1
+energy = 100
+position = 0
+while position != n:
+    if c[position] == 1:
+        energy -= 3
     else:
-        if pos +1 < n:
-            step += 1
-        pos += 1
+        energy -= 1
+    position += k
 
-print(step)
+print(energy)
