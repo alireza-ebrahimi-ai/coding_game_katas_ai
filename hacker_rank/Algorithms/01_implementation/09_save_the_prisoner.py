ef0f7cdfd8b21048ee96f68a1e385c33dfd18843
Dmitry Kustikov <waniz@mail.ru>
2016-08-08 12:27:59 +0300

    09_algorithm

--- hacker_rank/Algorithms/01_implementation/09_save_the_prisoner.py
+++ hacker_rank/Algorithms/01_implementation/09_save_the_prisoner.py
@@ -1,16 +1,34 @@
 """
 
 A jail has N prisoners, and each prisoner has a unique id number,
-S , ranging from 1 to N. There are M sweets that must be distributed to the prisoners.
+S, ranging from 1 to N. There are M sweets that must be distributed to the prisoners.
 
 1
-5 2 1
+5 3 5
 
 = 2
 """
 
 t = int(input().strip())
-n, m, s = map(int, input().strip().split(' '))
+num_cons = []
+num_sweet = []
+start_arr = []
+for i in range(0, t):
+    n, m, s = input().strip().split(' ')
+    num_cons.append(int(n))
+    num_sweet.append(int(m))
+    start_arr.append(int(s))
+
+for i in range(0, t):
+    position = (start_arr[i] + num_sweet[i] - 1) % num_cons[i]
+    if position == 0:
+        print(num_cons[i])
+    else:
+        print(position)
+
+
+
+
 
 
 
