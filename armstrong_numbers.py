361687021057b5da4dbf5dd854b8f31d3d6d8743
Dmitry Kustikov <waniz@mail.ru>
2016-08-08 13:28:14 +0300

    10 strange_counter

--- armstrong_numbers.py
+++ armstrong_numbers.py
@@ -3,7 +3,7 @@ A005188_list = []
 for k in range(1, 12):
     a = [i**k for i in range(10)]
     for b in combinations_with_replacement(range(10), k):
-        x = sum(map(lambda y:a[y], b))
+        x = sum(map(lambda y: a[y], b))
         if x > 0 and tuple(int(d) for d in sorted(str(x))) == b:
             A005188_list.append(x)
 A005188_list = sorted(A005188_list)
