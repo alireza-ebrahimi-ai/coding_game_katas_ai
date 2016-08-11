d898c6d65a7767aef9fd842c40dcba69c02a860f
Dmitry Kustikov <waniz@mail.ru>
2016-08-11 16:16:53 +0300

    some tiny additions

--- hacker_rank/Contests/week_of_code_22/02_matching_sets.py
+++ hacker_rank/Contests/week_of_code_22/02_matching_sets.py
@@ -4,7 +4,7 @@ week of code 22, task 3
 3
 1 2 3
 -1 4 3
-= -2
+= 2
 
 3
 1 2 3
@@ -15,6 +15,49 @@ week of code 22, task 3
 n = int(input().strip())
 x = [int(a_temp) for a_temp in input().strip().split(' ')]
 y = [int(a_temp) for a_temp in input().strip().split(' ')]
+
+x = sorted(x)
+y = sorted(y)
+steps = 0
+
+
+for i in range(n):
+    if x[i] > y[i]:
+        x[i] = x[i] - 1
+        changed = False
+        for j in range(i + 1, n):
+            if x[j] < y[j]:
+                x[j] = x[j] + 1
+                changed = True
+            if changed:
+                steps += 1
+                break
+            else:
+                x[i] = x[i] + 1
+
+    elif x[i] < y[i]:
+        x[i] = x[i] + 1
+        changed = False
+        for j in range(i, n):
+            if x[j] > y[j]:
+                x[j] = x[j] - 1
+                changed = True
+            if changed:
+                steps += 1
+                break
+            else:
+                x[i] = x[i] - 1
+
+print(sorted(x))
+print(sorted(y))
+if sorted(x) == sorted(y):
+    print(steps)
+else:
+    print(-1)
+
+
+
+
 
 
 
