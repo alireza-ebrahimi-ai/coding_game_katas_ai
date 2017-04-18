da28cb060a9930286e39c6b7eb4e18434a126421
DmitryKustikov <waniz@mail.ru>
2017-04-18 18:52:20 +0300

    warmup

--- hacker_rank/Contests/week_of_code_31/colliding_circles.py
+++ hacker_rank/Contests/week_of_code_31/colliding_circles.py
@@ -1,16 +1,30 @@
 import math
 from itertools import combinations
 
+
+def calc(r, k):
+    pi = math.pi
+
+    combi_list = []
+    combi_list.extend(combinations(r, k + 1))
+    comparator_sum = 0
+    for tuple_id in combi_list:
+        tuple_sum = sum(tuple_id)
+
+        missing_r = []
+        if k != n - 1:
+            for element in r:
+                if element not in tuple_id:
+                    missing_r.append(element ** 2 * pi)
+        comparator_sum += tuple_sum ** 2 * pi + sum(missing_r)
+    print(comparator_sum / len(combi_list))
+
+
 n, k = input().strip().split(' ')
 n, k = [int(n), int(k)]
 r = list(map(int, input().strip().split(' ')))
 
-Pi = math.pi
-
-combi_list = []
-combi_list.extend(combinations(r, k + 1))
-
-print(combi_list)
+calc(r, k)
 
 
 
--- hacker_rank/Contests/week_of_code_31/zero_one_game.py
+++ hacker_rank/Contests/week_of_code_31/zero_one_game.py
@@ -4,29 +4,34 @@ for a0 in range(g):
     n = int(input().strip())
     sequence = list(map(int, input().strip().split(' ')))
 
-    moves = 1
-    is_moves = True
-    while is_moves:
-        dels = 0
-        for i in range(1, len(sequence) - 1):
-            if len(sequence) == 2:
-                is_moves = False
-                if moves % 2 == 0:
-                    print('Alice')
-                else:
-                    print('Bob')
-            if i >= len(sequence):
+    moves = 0
+    flag = True
+    modify_arr = sequence[:]
+    while flag:
+        digits_taken = 0
+        for i in range(1, len(modify_arr)):
+
+            if len(modify_arr) - 1 <= i:
                 continue
-            if sequence[i - 1] == 0 and sequence[i + 1] == 0:
-                del sequence[i]
-                dels += 1
+
+            if len(modify_arr) == 2:
+                flag = False
+                if moves % 2 == 0:
+                    print('Bob')
+                else:
+                    print('Alice')
+
+            if modify_arr[i - 1] == 0 and modify_arr[i + 1] == 0:
+                del modify_arr[i]
+                digits_taken += 1
                 moves += 1
-        if dels == 0:
-            is_moves = False
+        if digits_taken == 0:
+            flag = False
+
     if moves % 2 == 0:
-        print('Alice')
-    else:
         print('Bob')
+    else:
+        print('Alice')
 
 
 
